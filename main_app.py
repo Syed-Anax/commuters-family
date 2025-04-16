bad67cc484d0dcc8e68d8f8fe4771a73de039327
Syed-Anax <oworld.pk@gmail.com>
2025-04-16 00:36:21 -0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,14 +1,13 @@
+# main_app.py
 import streamlit as st
 from carpool_app.splash_screens import show_splash_screens
 from carpool_app.login_signup import login_signup
 from homepage import show_homepage
 
 def main():
-    # Session state initialize
     if 'page' not in st.session_state:
         st.session_state.page = 'splash'
 
-    # Navigation logic
     if st.session_state.page == 'splash':
         show_splash_screens()
     elif st.session_state.page == 'auth':
