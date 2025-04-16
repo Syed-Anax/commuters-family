11c1bb3a932adb8c0374ac7bb9c5dad887b1910b
Syed-Anax <oworld.pk@gmail.com>
2025-04-16 01:05:00 -0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -4,9 +4,11 @@ from carpool_app.login_signup import login_signup
 from homepage import show_homepage
 
 def main():
+    # Session state initialize
     if 'page' not in st.session_state:
         st.session_state.page = 'splash'
 
+    # Navigation logic
     if st.session_state.page == 'splash':
         show_splash_screens()
     elif st.session_state.page == 'auth':
