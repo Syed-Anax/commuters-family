c592e04ed9d7bc4aea04d0fd9a9fd705c97a60c9
Syed-Anax <oworld.pk@gmail.com>
2025-04-19 08:23:28 -0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,24 +1,4 @@
-import streamlit as st
-from carpool_app.splash_screens import show_splash_screens
-from carpool_app.login_signup import login_signup
-from carpool_app.profile_setup import profile_setup
 from carpool_app.homepage import show_homepage
-from carpool_app.match_logic import match_logic
-
-def main():
-    if 'page' not in st.session_state:
-        st.session_state.page = 'splash'
-
-    if st.session_state.page == 'splash':
-        show_splash_screens()
-    elif st.session_state.page == 'auth':
-        login_signup()
-    elif st.session_state.page == 'setup':
-        profile_setup()
-    elif st.session_state.page == 'home':
-        show_homepage()
-    elif st.session_state.page == 'match':
-        match_logic()
 
 if __name__ == "__main__":
-    main()
+    show_homepage()
