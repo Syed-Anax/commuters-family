3349b4867ae3456d0b46635fa977ec8437388503
Syed-Anax <oworld.pk@gmail.com>
2025-05-02 07:49:23 -0700

    Update firebase_helper.py

--- utils/firebase_helper.py
+++ utils/firebase_helper.py
@@ -2,7 +2,6 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import streamlit as st
 
-# Initialize Firebase Admin
 if not firebase_admin._apps:
     cred = credentials.Certificate(dict(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred)
@@ -11,3 +10,10 @@ db = firestore.client()
 
 def save_user_profile(uid, data):
     db.collection('users').document(uid).set(data)
+
+def get_user_profile(phone_number):
+    doc = db.collection("users").document(phone_number).get()
+    if doc.exists:
+        return doc.to_dict()
+    else:
+        return None
