46b6a1667a881ffd07b0af717add7ce36c9ac66b
Syed-Anax <oworld.pk@gmail.com>
2025-05-02 10:01:53 -0700

    Update chat_helper.py

--- utils/chat_helper.py
+++ utils/chat_helper.py
@@ -1,1 +1,31 @@
+from firebase_admin import firestore
+db = firestore.client()
 
+def get_conversation_id(user1, user2):
+    return "_".join(sorted([user1, user2]))
+
+def send_message(sender, receiver, message):
+    convo_id = get_conversation_id(sender, receiver)
+    doc_ref = db.collection("messages").document(convo_id)
+    doc = doc_ref.get()
+    
+    if doc.exists:
+        messages = doc.to_dict().get("messages", [])
+    else:
+        messages = []
+    
+    messages.append({
+        "from": sender,
+        "to": receiver,
+        "text": message,
+        "timestamp": firestore.SERVER_TIMESTAMP
+    })
+
+    doc_ref.set({"messages": messages})
+
+def get_messages(user1, user2):
+    convo_id = get_conversation_id(user1, user2)
+    doc = db.collection("messages").document(convo_id).get()
+    if doc.exists:
+        return doc.to_dict().get("messages", [])
+    return []
