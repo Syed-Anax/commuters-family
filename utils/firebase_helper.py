de565c0c56284cf2085679ce2833311ec4cd91c9
Syed-Anax <oworld.pk@gmail.com>
2025-05-02 09:35:24 -0700

    Update firebase_helper.py

--- utils/firebase_helper.py
+++ utils/firebase_helper.py
@@ -17,3 +17,11 @@ def get_user_profile(phone_number):
         return doc.to_dict()
     else:
         return None
+def upgrade_to_premium(phone_number):
+    db.collection("users").document(phone_number).update({"is_premium": True})
+
+def check_if_premium(phone_number):
+    doc = db.collection("users").document(phone_number).get()
+    if doc.exists:
+        return doc.to_dict().get("is_premium", False)
+    return False
